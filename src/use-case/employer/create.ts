6d1601f3fafcc557ab0fe842d49e0c11d743989f
Anania Augusto <ananiasjaimeaugusto@gmail.com>
2025-02-04 16:29:22 +0100

    feat(usecase): add verification in user, if some data exists

--- src/use-case/employer/create.ts
+++ src/use-case/employer/create.ts
@@ -17,6 +17,12 @@ export type EmployerInputDTO = {
     created_at?: Date;
 }
 
+type verification = {
+    email: Employer | null,
+    telephone: Employer| null,
+    bi: Employer| null
+}
+
 export type EmployerOutputDTO = void;
 
 
@@ -34,10 +40,27 @@ export class CreateEmployerUsecase implements Usecase<EmployerInputDTO, Employer
 
             const aEmployer = await Employer.create(firstName, lastName, telephone, email, bi, id_store, this.storeGateway);
 
+            const emailAlreadyExists = await this.employerGateway.findByEmail(aEmployer.email);
+            const telephoneAlreadyExists = await this.employerGateway.findByTelephone(aEmployer.telephone);
+            const biAlreadyExists = await this.employerGateway.findByBi(aEmployer.bi);
+
+            const alreadyExists: verification = {
+                bi: biAlreadyExists,
+                email: emailAlreadyExists,
+                telephone: telephoneAlreadyExists
+            }
+
+            const verification: Array<keyof verification> = ["bi", "telephone", "email"];
+            for(const key of verification){
+                if(alreadyExists[key]){
+                    throw new Error(`${key} already exists`);
+                }
+            }
+
             await this.employerGateway.save(aEmployer);
 
         } catch (error) {
-            throw new Error("Something went wrong, we are fixing for you");
+            throw new Error(error.message);
         }
 
     }
