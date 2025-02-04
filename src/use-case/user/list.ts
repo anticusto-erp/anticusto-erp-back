fc6d7b95c7f5cb10729ca9e803427e6e3bd5b95a
Anania Augusto <ananiasjaimeaugusto@gmail.com>
2025-02-04 22:19:37 +0100

    feat: usecase user list

---
+++ src/use-case/user/list.ts
@@ -0,0 +1,46 @@
+import { User } from "../../domain/user/entity/user";
+import { UserGateway } from "../../domain/user/gateway/user.gateway";
+import { Usecase } from "../use-case";
+
+export type UserInputDTO = void;
+
+export type UserOutputDTO = {
+    
+    user: {
+        id: string;
+        username: string;
+        password?: string;
+        
+        id_employer: string;
+        id_access_role: string;
+    }
+    
+}
+
+export class ListUserUsecase implements Usecase<UserInputDTO, UserOutputDTO>{
+
+    public constructor(private readonly userGateway: UserGateway){}
+
+    public static create(userGateway: UserGateway){
+        return new ListUserUsecase(userGateway);
+    }
+
+    public async execute(): Promise<any> {
+        const aUser = await this.userGateway.list();
+        const output = this.present(aUser);
+
+        return output;
+    }
+
+    private present(input: User[]){
+        return input.map((input) => {
+            return {
+                id: input.id,
+                username: input.username,
+                id_employer: input.id_employer,
+                id_access_role: input.id_nivel_de_acesso
+            }
+        })
+    }
+
+}
