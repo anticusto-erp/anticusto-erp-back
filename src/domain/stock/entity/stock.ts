1951b1cd43b71440417df1a195893ff6c56fbaba
Anania Augusto <ananiasjaimeaugusto@gmail.com>
2025-02-09 10:04:02 +0100

    feat: create a entity

--- src/domain/stock/entity/stock.ts
+++ src/domain/stock/entity/stock.ts
@@ -1,15 +1,54 @@
+import { ProductGateway } from "../../product/gateway/product.gateway";
+
 export type StockProps = {
-    id: string;
+    id?: string;
     id_produto: string;
     quantidade: number;
-    created_at: string;
-    updated_at: string;
+    created_at?: string;
+    updated_at?: string;
 }
 
 export class Stock {
 
-    public constructor(props: StockProps){}
+    public constructor(private props: StockProps){}
 
-    public static create(id_produto: string, quantidade: number){}
+    public static create(id_produto: string, quantidade: number, productGateway: ProductGateway, updated_at?: string, id?: string){
+
+        const createdData = new Date().toISOString().split('T')[0];
+        const updatedData = new Date().toISOString().split('T')[0];
+
+        return new Stock({
+            id: id ?? crypto.randomUUID().toString(),
+            id_produto,
+            quantidade,
+            created_at: createdData,
+            updated_at: updated_at ?? updatedData
+        });
+
+    }
+
+    public whith (props: StockProps) {
+        return new Stock(props);
+    }
+
+    public get id(){
+        return this.props.id;
+    }
+
+    public get id_produto(){
+        return this.props.id_produto;
+    }
+
+    public get quantidade(){
+        return this.props.quantidade;
+    }
+
+    public get created_at(){
+        return this.props.created_at;
+    }
+
+    public get updated_at(){
+        return this.props.updated_at;
+    }
 
 }
--- src/domain/stock/gateway/stock.gateway.ts
+++ src/domain/stock/gateway/stock.gateway.ts
@@ -2,4 +2,7 @@ import {Stock} from "../entity/stock";
 
 export interface StockGateway {
     save(stock: Stock): Promise<void>;
+    list(): Promise<Stock[]>;
+    
+    findOne(id: string): Promise<Stock | null>;
 }
