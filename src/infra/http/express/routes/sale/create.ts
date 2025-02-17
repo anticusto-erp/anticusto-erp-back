c41c913b3ec0287ba391b6995d3e182c710722e4
Anania Augusto <ananiasjaimeaugusto@gmail.com>
2025-02-17 23:21:28 +0100

    feat: create sale usecase

---
+++ src/infra/http/express/routes/sale/create.ts
@@ -0,0 +1,7 @@
+export type saleInputDTO = {
+    id_cliente?: string;
+    id_usuario: string;
+    quantidade: number
+}
+
+export class CreateSaleRoute {}
---
+++ src/use-case/sale/create.ts
@@ -0,0 +1,31 @@
+import { Sale } from "../../domain/sale/entity/Sale";
+import { SaleGateway } from "../../domain/sale/gateway/sale.gateway";
+import { Usecase } from "../use-case";
+
+export type saleInputDTO = {
+    id_cliente?: string;
+    id_usuario: string;
+    quantidade: number
+}
+
+export type saleOutputDTO = void;
+
+export class CreateSaleUsecas implements Usecase<saleInputDTO, saleOutputDTO> {
+
+    public constructor(private readonly saleGateway: SaleGateway){}
+
+    public static create(saleGateway: SaleGateway){
+        return new CreateSaleUsecas(saleGateway);
+    }
+
+    public async execute({id_usuario, quantidade, id_cliente}: saleInputDTO): Promise<void> {
+
+        const aSale = Sale.create(id_usuario, quantidade, id_cliente);
+
+        await this.saleGateway.save(aSale);
+        
+
+    }
+
+}
+
